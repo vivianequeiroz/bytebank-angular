cd9ba42b6b0c5634a7402a9f4c819306bdb82bba
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-19 17:55:35 -0300

    chore: initial project structure + prettier + eslint

---
+++ src/app/new-transfer/new-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-transfer',
+  templateUrl: './new-transfer.component.html',
+  styleUrls: ['./new-transfer.component.scss']
+})
+export class NewTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
