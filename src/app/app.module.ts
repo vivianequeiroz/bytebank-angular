c90336eea50409aaa5e135923ab9d5592d126165
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-20 18:02:38 -0300

    chore: configuring app to work with pt-BR and URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from "@angular/core";
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { NewTransferComponent } from "./new-transfer/new-transfer.component";
-import { BankStatementComponent } from './bank-statement/bank-statement.component';
+import { BankStatementComponent } from "./bank-statement/bank-statement.component";
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
 
+registerLocaleData(localePt, "pt");
 @NgModule({
 	declarations: [AppComponent, NewTransferComponent, BankStatementComponent],
 	imports: [BrowserModule, FormsModule],
-	providers: [],
+	providers: [
+		{ provide: LOCALE_ID, useValue: "pt" },
+		{
+			provide: DEFAULT_CURRENCY_CODE,
+			useValue: "BRL",
+		},
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
